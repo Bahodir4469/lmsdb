6e3216f1529f0f02bbcef4f5b7ca1c160486f700
bahodirt3889 <bahodirt3889@gmail.com>
2025-10-20 03:46:27 -0700

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true, // 👈 BUNI ALBATTA QO‘Y
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {}
+};
